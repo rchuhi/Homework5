1d64f416cb9f6f399e80639a4b7927a83cb66653
rchuhi <94059813+rchuhi@users.noreply.github.com>
2021-11-29 16:53:21 -0500

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,29 @@
+/*Reginah Chuhi
+CIS219
+Tuesday and Thursday 3:30pm*/
+public class Switch {
+
+	public static void main(String[] args) {
+		/*Creates a variable named car 
+		 */
+		
+		String car = "Make";
+	    switch (car) {
+	      case "Toyota":
+	        System.out.println("The model is Corolla");
+	        break;
+	      case "landrover":
+	        System.out.println("The model is Freelander");
+	        break;
+	      case "Jeep":
+	        System.out.println("The model is Wrangler");
+	        break;
+	      case "Mazda":
+	        System.out.println("The model is CX-9");
+	        break;
+	      case "Make":
+	        System.out.println("This is the last option");
+	        break;
+	}
+//output This is the last option.
+	}}
